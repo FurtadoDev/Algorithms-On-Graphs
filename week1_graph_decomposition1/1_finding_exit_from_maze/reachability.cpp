1e249ca09d3cc47454e533bffacee71dde6438a1
FurtadoDev <vrfurtado@mun.ca>
2021-08-03 15:30:40 -0230

    added solns for week1

--- week1_graph_decomposition1/1_finding_exit_from_maze/reachability.cpp
+++ week1_graph_decomposition1/1_finding_exit_from_maze/reachability.cpp
@@ -5,22 +5,47 @@ using std::vector;
 using std::pair;
 
 
-int reach(vector<vector<int> > &adj, int x, int y) {
-  //write your code here
-  return 0;
+int dfs(vector<vector<int> >& adj, int src, int dst, vector<int> explored) {
+   
+    if(src == dst) return 1;
+
+    explored[src] = 1;
+
+    for (size_t i = 0; i < adj[src].size(); i++) {
+        if (explored[adj[src][i]] == 0)
+            return dfs(adj, adj[src][i], dst, explored);
+    }
+
+    return 0;
 }
 
+
+int reach(vector<vector<int> >& adj, int x, int y) {
+
+    vector<int> explored;
+    for (size_t i = 0; i < adj.size(); i++) {
+        explored.push_back(0);
+    }
+
+    int result = dfs(adj, x, y, explored);
+    explored.clear();
+   
+    return result;
+}
+
+
 int main() {
-  size_t n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (size_t i = 0; i < m; i++) {
+    size_t n, m;
+    std::cin >> n >> m;
+    vector<vector<int> > adj(n, vector<int>());
+    for (size_t i = 0; i < m; i++) {
+        int x, y;
+        std::cin >> x >> y;
+        adj[x - 1].push_back(y - 1);
+        adj[y - 1].push_back(x - 1);
+    }
     int x, y;
     std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  int x, y;
-  std::cin >> x >> y;
-  std::cout << reach(adj, x - 1, y - 1);
+    std::cout << reach(adj, x - 1, y - 1);
+    return 0;
 }
--- week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.cpp
+++ week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.cpp
@@ -4,21 +4,46 @@
 using std::vector;
 using std::pair;
 
-int number_of_components(vector<vector<int> > &adj) {
-  int res = 0;
-  //write your code here
-  return res;
+
+void dfs(vector<vector<int> >& adj, int node, vector<int>& explored) {
+    explored[node] = 1;
+    for (size_t i = 0; i < adj[node].size(); i++) {
+        if (explored[adj[node][i]] == 0) {
+            return dfs(adj, adj[node][i], explored);
+        }
+    }
+}
+
+int number_of_components(vector<vector<int> >& adj) {
+    int res = 0;
+   
+    size_t n = adj.size();
+    vector<int> explored;
+    for (int i = 0; i < n; i++) {
+        explored.push_back(0);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (explored[i] == 0) {
+            res += 1;
+            dfs(adj, i, explored);
+        }
+    }
+
+    explored.clear();
+   
+    return res;
 }
 
 int main() {
-  size_t n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (size_t i = 0; i < m; i++) {
-    int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  std::cout << number_of_components(adj);
+    size_t n, m;
+    std::cin >> n >> m;
+    vector<vector<int> > adj(n, vector<int>());
+    for (size_t i = 0; i < m; i++) {
+        int x, y;
+        std::cin >> x >> y;
+        adj[x - 1].push_back(y - 1);
+        adj[y - 1].push_back(x - 1);
+    }
+    std::cout << number_of_components(adj);
 }
