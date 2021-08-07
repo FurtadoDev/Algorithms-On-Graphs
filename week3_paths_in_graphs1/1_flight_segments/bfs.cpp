c7c2ae17817ff702f5e060f2f3e87b299cbeb59f
FurtadoDev <vrfurtado@mun.ca>
2021-08-07 12:44:08 -0230

    added soln for week3

--- week3_paths_in_graphs1/1_flight_segments/bfs.cpp
+++ week3_paths_in_graphs1/1_flight_segments/bfs.cpp
@@ -5,23 +5,46 @@
 using std::vector;
 using std::queue;
 
-int distance(vector<vector<int> > &adj, int s, int t) {
-  //write your code here
-  return -1;
+int distance(vector<vector<int> >& adj, int s, int t) {
+   
+    size_t n = adj.size(); //no of nodes
+    vector<int> dist(n, n + 1);
+    queue<int> q;
+    q.push(s);
+    dist[s] = 0;
+
+    while (!q.empty()) {
+
+        for (size_t i = 0; i < adj[q.front()].size(); i++) {
+            if (dist[adj[q.front()][i]] == n + 1) { //if the neighbour node hasn't been yet, update it's distance and put it in the queue
+                dist[adj[q.front()][i]] = dist[q.front()] + 1;
+                q.push(adj[q.front()][i]);
+            }
+        }
+       
+        q.pop();
+    }
+
+    if (dist[t] == n + 1)
+        return -1;
+    else
+        return dist[t];
 }
 
 int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  int s, t;
-  std::cin >> s >> t;
-  s--, t--;
-  std::cout << distance(adj, s, t);
+    int n, m;
+    std::cin >> n >> m;
+    vector<vector<int> > adj(n, vector<int>());
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        std::cin >> x >> y;
+        adj[x - 1].push_back(y - 1);
+        adj[y - 1].push_back(x - 1);
+    }
+    int s, t;
+    std::cin >> s >> t;
+    s--, t--;
+    std::cout << distance(adj, s, t);
+   
+    return 0;
 }
--- week3_paths_in_graphs1/2_bipartite/bipartite.cpp
+++ week3_paths_in_graphs1/2_bipartite/bipartite.cpp
@@ -5,20 +5,55 @@
 using std::vector;
 using std::queue;
 
-int bipartite(vector<vector<int> > &adj) {
-  //write your code here
-  return -1;
+int bipartite(vector<vector<int> >& adj) {
+   
+    size_t n = adj.size();
+    vector<int> dist(n, n + 1);
+    queue<int> q;
+    dist[0] = 0;
+    q.push(0);
+
+    while (!q.empty()) {
+        //put all of its neighbours in a queue
+        for (size_t j = 0; j < adj[q.front()].size(); j++) {
+            //if the node is not yet processed
+            if (dist[adj[q.front()][j]] == n + 1) {
+
+                dist[adj[q.front()][j]] = dist[q.front()] + 1;
+                q.push(adj[q.front()][j]);
+
+            }
+            else { //if the node has already been processed
+
+                if ((dist[adj[q.front()][j]] - dist[q.front()]) % 2 == 0) {
+                    while (!q.empty()) {
+                        q.pop();
+                    }
+
+                    return 0;
+                }
+
+            }
+
+        }
+
+        q.pop();
+    }
+
+    return 1;
 }
 
 int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  std::cout << bipartite(adj);
+    int n, m;
+    std::cin >> n >> m;
+    vector<vector<int> > adj(n, vector<int>());
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        std::cin >> x >> y;
+        adj[x - 1].push_back(y - 1);
+        adj[y - 1].push_back(x - 1);
+    }
+    std::cout << bipartite(adj);
+   
+    return 0;
 }
